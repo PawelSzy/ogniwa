630ec8d250430dcbe385f539a9c510d16aef09d5
PawelSzy <peal1982@gmail.com>
2017-10-25 23:10:12 +0200

    Kasowanie po kliknieciu

--- calculate_nors.js
+++ calculate_nors.js
@@ -12,17 +12,35 @@ $("#calculate_nors").click(function(e) {
     console.log(nors);
 
     var sum = parseInt($("#sum_input").val());
-
     var nors_table = calculate_nors(sum, nors);
+    show_calculatated_nors(sum, nors_table);
+});
+
+function show_calculatated_nors(sum, nors_table) {
+
     console.log(nors_table);
 
     $("#table_of_ogniwa").empty();
-    nors_table.forEach(function(calc_nors) {
+    nors_table.forEach(function (calc_nors, index) {
         var nors_string = calc_nors.join();
+        var closure_index = index;
+        $("#table_of_ogniwa").append(function() {
+            return $("<tr class='nors_row'  id='nors_row_'" + index + ">"
+            + "<td>"
+            + calc_nors.join(" + ")
+            + " = " + sum
+            + "</td></tr>"
+            ).click(function() {
+                console.log('index', index);
+                show_calculatated_nors(sum, nors_table.filter(function(elem, nor_index) {
+                    return nor_index != index;
+                }))
+            })
+        })
+    });
 
-        $("#table_of_ogniwa").append("<tr><td>"
-        + calc_nors.join(" + ")
-        + " = " + sum
-        + "</td></tr>");
-    })
-});
+    //$('.nors_row').unbind();
+    //$('.nors_row').click(function () {
+    //    $index$(this).attr('id');
+    //});
+}
