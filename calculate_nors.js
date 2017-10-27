20cb97e89eeb231e8a6333e09dd0f6f756a3fa16
PawelSzy <peal1982@gmail.com>
2017-10-27 20:50:04 +0200

    Popraw wyglad tabel

--- calculate_nors.js
+++ calculate_nors.js
@@ -9,7 +9,6 @@ $("#calculate_nors").click(function(e) {
         .map(function(elem){
             return parseInt(elem);
         });
-    console.log(nors);
 
     var sum = parseInt($("#sum_input").val());
     var calculated_nors_table = calculate_nors(sum, nors);
@@ -17,10 +16,8 @@ $("#calculate_nors").click(function(e) {
 });
 
 function show_calculatated_nors(sum, nors, calculated_nors_table) {
-
-    console.log(calculated_nors_table);
-
     $("#table_of_ogniwa").empty();
+
     calculated_nors_table.forEach(function (calc_nors, index) {
         var nors_string = calc_nors.join();
         var closure_index = index;
@@ -31,35 +28,17 @@ function show_calculatated_nors(sum, nors, calculated_nors_table) {
             + " = " + sum
             + "</td></tr>"
             ).click(function() {
-                console.log('index', index);
                 var closure_index = index;
-                $('#table_of_chosen_ogniwa').append($(this));
+                $('#table_of_chosen_ogniwa').append($(this)).unbind();
 
-                console.log('nors_tab', calculated_nors_table.filter(function(elem, nor_index) {
-                    return nor_index != closure_index;
-                }));
-
-                console.log('calc_nors', calc_nors);
-
-                console.log('nors1', nors);    
-                    
                 calc_nors.forEach(function(nors_value){
-                    console.log('nors_value', nors_value);
                     nors = delete_value_from_nors(nors, nors_value)
                 });
 
-                console.log('nors2', nors);
-                console.log('sum', sum);
-                console.log('nors_calc', calculate_nors(sum, nors));
                 show_calculatated_nors(sum, nors, calculate_nors(sum, nors));
             })
         })
     });
-
-    //$('.nors_row').unbind();
-    //$('.nors_row').click(function () {
-    //    $index$(this).attr('id');
-    //});
 }
 
 
@@ -73,6 +52,4 @@ function find_and_delete_from_array(value, arr) {
     if (index > -1) {
         arr.splice(index, 1);
     }
-    
-    console.log('ind', arr);
 }
